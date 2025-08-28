db0305b6abc7c3448d83669a0bc01a67dcb849ff
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-08-28 05:38:20 +0100

    .

--- frontend/src/app/dashboard/columns.jsx
+++ frontend/src/app/dashboard/columns.jsx
@@ -8,8 +8,6 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
-import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import {
     Select,
     SelectContent,
