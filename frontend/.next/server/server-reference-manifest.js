3917891ad7b705a1603fbd123afb3ba8bfdd974d
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-09-20 11:18:50 +0100

    Delete frontend dir from backend branch

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3KYGaTXL5Qb8ChabFcsKPAmWRo+9WKYOAtXK3EybmWs=\"\n}"
--- frontend/.next/static/chunks/src_app_layout_0a548d63.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__a3dce129._.css",
-  "static/chunks/node_modules_next_dist_b47712da._.js",
-  "static/chunks/src_app_authProvider_jsx_61e85a4c._.js"
-],
-    source: "dynamic"
-});
