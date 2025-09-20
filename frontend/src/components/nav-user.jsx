0c958a7a8436d65f59b79edd09af0c44bd28b99e
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-09-20 10:51:38 +0100

    Add time selection for only hour selection

--- frontend/src/components/nav-user.jsx
+++ frontend/src/components/nav-user.jsx
@@ -69,7 +69,7 @@ export function NavUser() {
                         <DropdownMenuLabel className="p-0 font-normal">
                             <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                                 <Avatar className="h-8 w-8 rounded-lg">
-                                    <AvatarImage src={user?.avatar} alt={user?.username} />
+                                    <AvatarImage src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${user?.profileImage}`} alt={user?.username} />
                                     <AvatarFallback className="rounded-lg">{user?.username.slice(0, 2).toUpperCase()}</AvatarFallback>
                                 </Avatar>
                                 <div className="grid flex-1 text-left text-sm leading-tight">
