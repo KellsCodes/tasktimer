3678cc017a52d4a14f2fca096c7e42c790165879
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-09-30 01:48:39 +0100

    add Google auth

--- frontend/src/components/nav-user.jsx
+++ frontend/src/components/nav-user.jsx
@@ -49,7 +49,11 @@ export function NavUser() {
                             className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                         >
                             <Avatar className="h-8 w-8 rounded-full">
-                                <AvatarImage src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${user?.profileImage}`} alt={user?.username} />
+                                <AvatarImage
+                                    src={user?.profileImage && !/storage/.test(user?.profileImage) ?
+                                        user?.profileImage : `${process.env.NEXT_PUBLIC_API_BASE_URL}/${user?.profileImage}`}
+                                    alt={user?.username}
+                                />
                                 <AvatarFallback className="rounded-full">{user?.username.slice(0, 2).toUpperCase()}</AvatarFallback>
 
                             </Avatar>
@@ -69,7 +73,11 @@ export function NavUser() {
                         <DropdownMenuLabel className="p-0 font-normal">
                             <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                                 <Avatar className="h-8 w-8 rounded-lg">
-                                    <AvatarImage src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${user?.profileImage}`} alt={user?.username} />
+                                    <AvatarImage
+                                        src={user?.profileImage && !/storage/.test(user?.profileImage) ?
+                                            user?.profileImage : `${process.env.NEXT_PUBLIC_API_BASE_URL}/${user?.profileImage}`}
+                                        alt={user?.username}
+                                    />
                                     <AvatarFallback className="rounded-lg">{user?.username.slice(0, 2).toUpperCase()}</AvatarFallback>
                                 </Avatar>
                                 <div className="grid flex-1 text-left text-sm leading-tight">
