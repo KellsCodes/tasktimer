de28e959004f4556ab300a32cd1220fdefe7766b
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-10-09 14:54:13 +0100

    debugging

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -345,9 +345,9 @@ export const loginWithGoogleCallback = async (
     });
 
     const cookieOption = {
-      httpOnly: true,
+      httpOnly: process.env.NODE_ENV === "production" && true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     };
     res.cookie("accessToken", accessToken, cookieOption);
